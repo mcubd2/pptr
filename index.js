f40a2832c1ccda7e3a5a2e8c50370d72515697dc
zayn <mdrahul6967@gmail.com>
2022-12-02 03:34:37 +0600

    hjh

--- index.js
+++ index.js
@@ -8,7 +8,7 @@
 // import bodyParser from 'body-parser'
 
 const express = require("express");
-const bodyParser = require("body-parser");
+// const bodyParser = require("body-parser");
 const cors = require("cors");
 
 
@@ -86,7 +86,7 @@ app.use(cors({
 }));
 app.set('trust proxy', true)
 // app.use(express.json())
-app.use(bodyParser.text({ type: "*/*" }));
+// app.use(bodyParser.text({ type: "*/*" }));
 
 
 
@@ -157,7 +157,7 @@ var bgfind = async (fblink) => {
 app.post("/", async (req, res) => {
 
 
-  res.send(await bgfind(req.body));
+  res.send(await bgfind(req.headers.data));
 
 });
 
