1620c2cbe989d52f7372677484a950e8627d060f
zayn <mdrahul6967@gmail.com>
2022-12-04 03:46:35 +0600

    hjh

--- index.js
+++ index.js
@@ -123,9 +123,9 @@ var bgfind = async (fblink) => {
 
 
 
-    await page.waitForSelector('img', {
-      visible: true,
-    })
+    // await page.waitForSelector('img', {
+    //   visible: true,
+    // })
 
 
     const data = await page.evaluate(() => {
