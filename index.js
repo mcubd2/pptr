afd8ceb66cfb5a0f388a82a78e83b298eb2f8924
zayn <mdrahul6967@gmail.com>
2022-12-04 03:57:04 +0600

    hjh

--- index.js
+++ index.js
@@ -136,7 +136,7 @@ var bgfind = async (fblink) => {
       const objj = Object.assign({}, urls);
 
 
-      return document.querySelector('body').innerHTML
+      return objj
     })
 
     return data
