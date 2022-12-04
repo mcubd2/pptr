ba27fa489c5119e32c2b017d80b005ab9153b35b
zayn <mdrahul6967@gmail.com>
2022-12-05 03:02:58 +0600

    hjh

--- index.js
+++ index.js
@@ -14,16 +14,7 @@ const cors = require("cors");
 
 
 
-let chrome = {};
-let puppeteer;
 
-
-if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
-  chrome = require("chrome-aws-lambda");
-  puppeteer = require("puppeteer-core");
-} else {
-  puppeteer = require("puppeteer");
-}
 
 
 
@@ -34,49 +25,6 @@ const app = express();
 const PORT = 3000;
 
 
-// async function get(url){
-//     try{
-//         const res=await axios.get(url)
-//         const $= cheerio.load(res.data)
-//         const genre=$('body').text()
-//         console.log(genre)
-//         console.log(res)
-
-
-
-//         return genre
-
-//     }
-//     catch(eror){
-//         console.error(eror)
-//     }
-// }
-
-// var options = {
-//     url: 'https://www.facebook.com/itsnayan007',
-//     headers: {
-//       'content-type': 'application/json',
-//       'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'
-//     },
-
-//   };
-
-// var u
-// request(options,(eror,response,html)=>{
-
-// if(!eror && response.statusCode==200){
-//     const $=cheerio.load(html)
-//     // const final=$('body').children('div').html()
-//     const final=$('body').html()
-//     console.log(final)
-//      u =final
-// }
-
-
-// })
-
-
-// get('https://facebook.com')
 
 app.listen(process.env.PORT || PORT, () => {//    console.log(`Server is running on PORT: ${PORT}`);
 });
@@ -91,67 +39,7 @@ app.use(cors({
 
 
 
-var bgfind = async (fblink) => {
 
-  try {
-    const regex = /^.+facebook/;
-    const fblinkregex = fblink.replace(regex, 'https://www.facebook');
-
-
-    let options = {};
-
-    if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
-      options = {
-        args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
-        defaultViewport: chrome.defaultViewport,
-        executablePath: await chrome.executablePath,
-        headless: true,
-        ignoreHTTPSErrors: true,
-      };
-    }
-
-
-
-
-
-
-
-    const browser = await puppeteer.launch(options);
-    const page = await browser.newPage();
-    await page.goto(fblink);
-
-
-
-
-
-    // await page.waitForSelector('img', {
-    //   visible: true,
-    // })
-
-
-    const data = await page.evaluate(() => {
-      const images = document.querySelectorAll('img');
-
-      const urls = Array.from(images).map(v => v.src);
-
-      const objj = Object.assign({}, urls);
-
-
-      return objj
-    })
-
-    return data
-
-
-  }
-  catch (eror) {
-    console.error(eror)
-    return 'eror ' + eror
-  }
-
-
-
-}
 
 
 
@@ -163,7 +51,7 @@ var bgfind = async (fblink) => {
 app.post("/", async (req, res) => {
 
 
-  res.send(await bgfind(req.headers.data));
+  res.send('kk');
 
 });
 
--- mainn.js
+++ mainn.js
@@ -7,77 +7,172 @@
 // import cors from 'cors'
 // import bodyParser from 'body-parser'
 
-
-import got from 'got';
-import tunnel from 'tunnel';
-import https from 'https';
-
+const express = require("express");
+// const bodyParser = require("body-parser");
+const cors = require("cors");
 
 
 
 
-// -------------------------------------------------------------------
-
-// -------------------------------------------------------------------
-
+let chrome = {};
+let puppeteer;
 
 
-const apiurl = 'https://mcubd1.netlify.app/';
-const urlA = [...new Array(2)].map((u) => apiurl);
+if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+  chrome = require("chrome-aws-lambda");
+  puppeteer = require("puppeteer-core");
+} else {
+  puppeteer = require("puppeteer");
+}
 
-const procreq = async ({ url }) => {
+
+
+
+
+
+const app = express();
+const PORT = 3000;
+
+
+// async function get(url){
+//     try{
+//         const res=await axios.get(url)
+//         const $= cheerio.load(res.data)
+//         const genre=$('body').text()
+//         console.log(genre)
+//         console.log(res)
+
+
+
+//         return genre
+
+//     }
+//     catch(eror){
+//         console.error(eror)
+//     }
+// }
+
+// var options = {
+//     url: 'https://www.facebook.com/itsnayan007',
+//     headers: {
+//       'content-type': 'application/json',
+//       'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'
+//     },
+
+//   };
+
+// var u
+// request(options,(eror,response,html)=>{
+
+// if(!eror && response.statusCode==200){
+//     const $=cheerio.load(html)
+//     // const final=$('body').children('div').html()
+//     const final=$('body').html()
+//     console.log(final)
+//      u =final
+// }
+
+
+// })
+
+
+// get('https://facebook.com')
+
+app.listen(process.env.PORT || PORT, () => {//    console.log(`Server is running on PORT: ${PORT}`);
+});
+
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH']
+}));
+// app.set('trust proxy', true)
+// app.use(express.json())
+// app.use(bodyParser.text({ type: "*/*" }));
+
+
+
+var bgfind = async (fblink) => {
+
   try {
-    const res = await got(url);
-    return {
-      status: 'success',
-      statusCode: res.statusCode,
-    };
-  } catch (err) {
-    return { status: 'error', message: err + '' };
-  }
-};
-
-const run = async () => {
-  try {
-    const results = await Promise.all(urlA.map((url) => procreq({ url })));
-    console.table(results);
-
-  } catch (err) {
-    console.error(err);
-  }
-};
-
-// run(); 
+    const regex = /^.+facebook/;
+    const fblinkregex = fblink.replace(regex, 'https://www.facebook');
 
 
+    let options = {};
 
- 
-
-
- async function  a(){
- try{
-
-  var tunnelingAgent = tunnel.httpsOverHttp({
-    proxy: {
-      host: 'localhost',
-      port: 3128
+    if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+      options = {
+        args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
+        defaultViewport: chrome.defaultViewport,
+        executablePath: await chrome.executablePath,
+        headless: true,
+        ignoreHTTPSErrors: true,
+      };
     }
-  });
-   
-  var req = https.request({
-    host: 'example.com',
-    port: 443,
-    agent: tunnelingAgent
-  });
-
-console.log(req)
 
 
- }
- catch(e){
-  console.log(e)
- }
- }
 
-a()
+
+
+
+
+    const browser = await puppeteer.launch(options);
+    const page = await browser.newPage();
+    await page.goto(fblink);
+
+
+
+
+
+    // await page.waitForSelector('img', {
+    //   visible: true,
+    // })
+
+
+    const data = await page.evaluate(() => {
+      const images = document.querySelectorAll('img');
+
+      const urls = Array.from(images).map(v => v.src);
+
+      const objj = Object.assign({}, urls);
+
+
+      return objj
+    })
+
+    return data
+
+
+  }
+  catch (eror) {
+    console.error(eror)
+    return 'eror ' + eror
+  }
+
+
+
+}
+
+
+
+
+
+
+
+
+app.post("/", async (req, res) => {
+
+
+  res.send(await bgfind(req.headers.data));
+
+});
+
+app.get("/", async (req, res) => {
+
+  // console.log(req.headers)
+  res.send('Home sweat home!');
+});
+
+
+
 
