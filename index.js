44d64430600aaa47c5ae89957d96ae2f455858ac
mcubd <106980362+mcubd@users.noreply.github.com>
2023-10-11 02:58:46 +0600

    Update index.js

--- index.js
+++ index.js
@@ -259,12 +259,13 @@ var gdrive = async (gdlink) => {
 
 
     const browser = puppeteer.launch(options);
-return "hsldk"
+
     const createInstance = async (url) => {
       let real_instance = await browser;
       let page = await real_instance.newPage();
       await page.goto("https://drive.google.com/u/4/uc?id=1PEkrNN4T2ZoqwDrpMU9Oeq8Go8AytqLw&export=download");
       // await page.waitForSelector('form', { visible: true, })
+     return "hdkdop"
       const data = await page.evaluate(() => {
 
         return "document.getElementsByTagName()"
