02177bc9bf76a32a3bdbe285ff29fadd8047c97b
mcubd <106980362+mcubd@users.noreply.github.com>
2023-10-15 01:50:16 +0600

    Update index.js

--- index.js
+++ index.js
@@ -267,7 +267,7 @@ var url="https://youtu.be/dXjKh66BR2U?si=FvuTvalLS34CJhYq"
     await page.goto("https://en.savefrom.net/");
       
     await page.waitForSelector('div', { visible: true, })
-     await page.type('[name=url]',a)
+     await page.type('[name=sf_url]',a)
     await page.click('[type=submit]')
     await page.waitFor(10000)
       const data = await page.evaluate(async () => {
