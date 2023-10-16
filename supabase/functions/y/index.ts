2deb4c7d9165f0fc512b450c9f957091509bca64
mcubd <106980362+mcubd@users.noreply.github.com>
2023-10-16 13:44:05 +0600

    Update index.ts

--- supabase/functions/y/index.ts
+++ supabase/functions/y/index.ts
@@ -3,46 +3,38 @@
 // This enables autocomplete, go to definition
 // To invoke: 
 import { serve }from "https://deno.land/std@0.204.0/http/server.ts";
-//import puppeteer from "https://deno.land/x/puppeteer@16.2.0/mod.ts"
-import * as pptr from "https://deno.land/x/pptr@1.2.0/mod.ts";
+import puppeteer from "https://deno.land/x/puppeteer@16.2.0/mod.ts"
+//import * as pptr from "https://deno.land/x/pptr@1.2.0/mod.ts";
  
 
 serve(async (req) => { 
   try 
   { 
     // console.log('PUPPETEER_BROWSERLESS_IO_KEY') 
-      // const browser = await puppeteer.connect({ 
-      //  browserWSEndpoint: `wss://chrome.browserless.io?token=677cf9f1-7c6f-4a8e-876e-6e0762f556f5` 
-      //})   
+      const browser = await puppeteer.connect({ 
+       browserWSEndpoint: `wss://chrome.browserless.io?token=677cf9f1-7c6f-4a8e-876e-6e0762f556f5` 
+      })   
 
    const browser = await pptr.launch({
-  executablePath: '/usr/bin/google-chrome',
-});
-
-const page = await browser.newPage();
-
-await page.goto('https://google.com');
-await page.screenshot();
-
-// await browser.close();
-      // const page = await browser.newPage()   
-   // await page.setRequestInterception(true);
-  //  page.on('request', (request) => {
+  
+      const page = await browser.newPage()   
+   await page.setRequestInterception(true);
+  // page.on('request', (request) => {
     // if (['image', 'stylesheet', 'font'].indexOf(request.resourceType()) !== -1) {
      //   request.abort();
    // } else {
      //   request.continue();
   //  }
 // });
-      //  const url = new URL(req.url).searchParams.get('url') || 'https://www.videovor.com/'  
-       //   await page.goto(url) 
- //  await page.waitForSelector('input')
-   //const n = await page.$("li[class='heading']")
-  //await page.type('#url','https://youtu.be/dXjKh66BR2U?si=FvuTvalLS34CJhYq')
+       const url = new URL(req.url).searchParams.get('url') || 'https://www.videovor.com/'  
+        await page.goto(url) 
+  await page.waitForSelector('input')
+   const n = await page.$("li[class='heading']")
+  await page.type('#url','https://youtu.be/dXjKh66BR2U?si=FvuTvalLS34CJhYq')
    
 //    await page.click('[type=submit]')
 //   await page.waitForTimeout(3000)
-         //    const screenshot = await page.screenshot()  
+           const screenshot = await page.screenshot()  
               return new Response(screenshot, { headers: { 'Content-Type': 'image/png' } }) } 
                catch (e) { 
                 console.error(e)  
