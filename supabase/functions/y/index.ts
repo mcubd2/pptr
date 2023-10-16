5434d6288aa771679ae3e3528a47adc5c43e4bc1
mcubd <106980362+mcubd@users.noreply.github.com>
2023-10-16 16:30:41 +0600

    Update index.ts

--- supabase/functions/y/index.ts
+++ supabase/functions/y/index.ts
@@ -4,7 +4,7 @@
 // This enables autocomplete, go to definition
 // To invoke: 
 import { serve }from "https://deno.land/std@0.204.0/http/server.ts";
-import puppeteer from "https://deno.land/x/puppeteer@16.2.0/mod.ts"
+import puppeteer from "https://deno.land/x/puppeteer@9.0.0/mod.ts"
 //import * as puppeteer from "https://deno.land/x/puppeteer_plus/mod.ts";
 //import * as pptr from "https://deno.land/x/pptr@1.2.0/mod.ts";
  
