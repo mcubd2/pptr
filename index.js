12d1c425c2eb0a66a1939bdc9da4cde156020521
mcubd <106980362+mcubd@users.noreply.github.com>
2023-10-17 01:11:48 +0600

    Update index.js

--- index.js
+++ index.js
@@ -257,8 +257,8 @@ chromium.setHeadlessMode = true;
     const browser = await puppeteer.launch({
     args: chromium.args,
     defaultViewport: chromium.defaultViewport,
-    executablePath: await chromium.executablePath(),
-    headless: chromium.headless,})
+   // executablePath: await chromium.executablePath() ,
+      headless: chromium.headless,})
     
     const page = await browser.newPage();
   await page.goto("https://www.google.com");
