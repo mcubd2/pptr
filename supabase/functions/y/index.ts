55efd6a4606f9327ef9b82f05912dd46212cde42
mcubd <106980362+mcubd@users.noreply.github.com>
2023-10-16 15:43:35 +0600

    Update index.ts

--- supabase/functions/y/index.ts
+++ supabase/functions/y/index.ts
@@ -43,9 +43,9 @@ serve(async (req) => {
 //   await page.waitForTimeout(3000)
       //  import puppeteer from "https://deno.land/x/puppeteer@16.2.0/mod.ts";
 
-//-------const browser = await puppeteer.launch();
-//-------const page = await browser.newPage();
-//--------await page.goto("https://example.com");
+const browser = await puppeteer.launch();
+const page = await browser.newPage();
+await page.goto("https://example.com");
 //await page.screenshot({ path: "example.png" });
 
 
