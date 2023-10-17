515373ae131080142bd6ab937620ced701451d57
mcubd <106980362+mcubd@users.noreply.github.com>
2023-10-17 08:32:15 +0600

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@
 // import puppeteer from 'puppeteer'
 // import cors from 'cors' 
 // import bodyParser from 'body-parser'
-  
+
 const express = require("express");
 //const mongoose = require("mongoose");
 const cors = require("cors");
@@ -31,9 +31,9 @@ let chrome = {};
 if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
   console.log("----------------running-on-lamda------------")
   //chrome = require("chrome-aws-lambda");
-//  puppeteer = require("puppeteer-core");
+  //  puppeteer = require("puppeteer-core");
 } else {
- // puppeteer = require("puppeteer");
+  // puppeteer = require("puppeteer");
 }
 
 const app = express();
@@ -233,7 +233,7 @@ var bgfind3 = async (fblink) => {
     }
    var v= await Promise.all(arr)
    
-
+ 
     return v
 
   }
@@ -248,146 +248,146 @@ var bgfind3 = async (fblink) => {
 */
 
 //---------------------
-   var gdrive = async (gdlink) => {
+var gdrive = async (gdlink) => {
 
-  
 
-    try{
-chromium.setHeadlessMode = true;
+
+  try {
+    chromium.setHeadlessMode = true;
     chromium.setGraphicsMode = false;
     const browser = await puppeteer.launch({
-    args: chromium.args,
-    defaultViewport: chromium.defaultViewport,
-    executablePath: await chromium.executablePath("https://github.com/Sparticuz/chromium/releases/download/v110.0.1/chromium-v110.0.1-pack.tar") ,
-     headless: chromium.headless,
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath("https://github.com/Sparticuz/chromium/releases/download/v110.0.1/chromium-v110.0.1-pack.tar"),
+      headless: chromium.headless,
     })
-    
+
     const page = await browser.newPage();
-  await page.goto("https://x2mate.com/");
+    await page.goto("https://x2mate.com/");
     await page.waitForTimeout(3000)
-    await page.type('[name=s_input]','url')
-    const screenshot = await page.screenshot({ path: 's.png' })  
+    await page.type('#getlinks', 'url')
+    //const screenshot = await page.screenshot({ path: 's.png' })
     return screenshot
-    
-  const p = await page.title();
-  await browser.close();
-return p
 
-    }catch(err){
-      return JSON.stringify({ error: err.message })
-    }
+    const p = await page.title();
+    await browser.close();
+    return p
 
-    
-   // console.log("step---------------------")
-   /* let options = {};
-    if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
-      options = {
-        args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
-        defaultViewport: chrome.defaultViewport,
-        executablePath: await chrome.executablePath,
-        headless: true,
-        ignoreHTTPSErrors: true,
-      };
-    }
-    console.log("step------------1---------")
-    var arr=[]
+  } catch (err) {
+    return JSON.stringify({ error: err.message })
+  }
+
+
+  // console.log("step---------------------")
+  /* let options = {};
+   if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+     options = {
+       args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
+       defaultViewport: chrome.defaultViewport,
+       executablePath: await chrome.executablePath,
+       headless: true,
+       ignoreHTTPSErrors: true,
+     };
+   }
+   console.log("step------------1---------")
+   var arr=[]
 var url='https://youtu.be/dXjKh66BR2U?si=FvuTvalLS34CJhYq'
 
 
-    
-    const browser = await puppeteer.launch(options);
-    const page = await browser.newPage();
-    //console.log("step------------2--------")
-      //let real_instance = await browser;
-     // let page = await real_instance.newPage();
-    await page.setRequestInterception(true);
-    page.on('request', (request) => {
-    if (['image', 'stylesheet', 'font'].indexOf(request.resourceType()) !== -1) {
-        request.abort();
-    } else {
-        request.continue();
-    }
+   
+   const browser = await puppeteer.launch(options);
+   const page = await browser.newPage();
+   //console.log("step------------2--------")
+     //let real_instance = await browser;
+    // let page = await real_instance.newPage();
+   await page.setRequestInterception(true);
+   page.on('request', (request) => {
+   if (['image', 'stylesheet', 'font'].indexOf(request.resourceType()) !== -1) {
+       request.abort();
+   } else {
+       request.continue();
+   }
 });
-    console.log("step------------b-goto--------")
-    await page.goto("https://en.savefrom.net/");
-      console.log("step------------a-goto--------")
-    await page.waitFor(3000)
-    
-    var xhttpop = new XMLHttpRequest();
-     xhttpop.open("POST", 'https://gifted-pear-loincloth.cyclic.cloud/chatnot', true);
-     xhttpop.send("after waitfor");
-    
-      console.log("step------------a-waitfor--------")
-     await page.type('[name=sf_url]',url)
-   // await page.type(JSON.stringify(url));
-    await page.click('[type=submit]')
-    await page.screenshot({ path: 's.png' });
-   console.log("------------scrernshot------------")
-    await page.close();
-    console.log("done--------------")
-    return browser
+   console.log("step------------b-goto--------")
+   await page.goto("https://en.savefrom.net/");
+     console.log("step------------a-goto--------")
+   await page.waitFor(3000)
+   
+   var xhttpop = new XMLHttpRequest();
+    xhttpop.open("POST", 'https://gifted-pear-loincloth.cyclic.cloud/chatnot', true);
+    xhttpop.send("after waitfor");
+   
+     console.log("step------------a-waitfor--------")
+    await page.type('[name=sf_url]',url)
+  // await page.type(JSON.stringify(url));
+   await page.click('[type=submit]')
+   await page.screenshot({ path: 's.png' });
+  console.log("------------scrernshot------------")
+   await page.close();
+   console.log("done--------------")
+   return browser
 console.log("ghsjsjsjjsjsjsjg-")
 
-    
-    await page.waitFor(15000)
-    //await page.waitForSelector('a', { visible: true, })
-    await page.screenshot({ path: 's.png' });
-      const data = await page.evaluate(async () => {
-        
-        var divs = document.querySelectorAll('a')
-        const urls =Array.from(divs).map(v => v.href)
-        return urls
-      })
-    await page.close();
-      return data
-
-    const createInstance = async (url) => {
-      
-      let real_instance = await browser;
-      let page = await real_instance.newPage();
-      
-      
-      await page.goto("https://drive.google.com/u/4/uc?id=1PEkrNN4T2ZoqwDrpMU9Oeq8Go8AytqLw&export=download");
-       await page.waitForSelector('form', { visible: true, })
-      
-      const data = await page.evaluate(() => {
-
-        return "document.getElementsByTagName()"
-        const ar = []
-        for (const i of document.getElementById('cont').children) {
-          console.log(i.getElementsByClassName('link')[0].innerText)
-          ar.push(i.getElementsByClassName('link')[0].innerText)
-        }
-
-        return ar
-      })
-
-
-      await page.close();
-      return data
-    }
-
-   return  createInstance();
-    
-*/
-    // add tasks to queue
-    var urls=['j','k','m']
-    for (let i of urls) {
-      if(i=='j'){
-        arr.push(createInstance('https://mcubd.netlify.app/'))
-      }else if(i=='k'){
-        arr.push(createInstance('https://mcubd.netlify.app/marvel/'))
-      }else{
-        arr.push(createInstance('https://mcubd.netlify.app/others'))
-      }
-    }
-   var v= await Promise.all(arr)
    
+   await page.waitFor(15000)
+   //await page.waitForSelector('a', { visible: true, })
+   await page.screenshot({ path: 's.png' });
+     const data = await page.evaluate(async () => {
+       
+       var divs = document.querySelectorAll('a')
+       const urls =Array.from(divs).map(v => v.href)
+       return urls
+     })
+   await page.close();
+     return data
 
-    return v
+   const createInstance = async (url) => {
+     
+     let real_instance = await browser;
+     let page = await real_instance.newPage();
+     
+     
+     await page.goto("https://drive.google.com/u/4/uc?id=1PEkrNN4T2ZoqwDrpMU9Oeq8Go8AytqLw&export=download");
+      await page.waitForSelector('form', { visible: true, })
+     
+     const data = await page.evaluate(() => {
 
-  
-  
+       return "document.getElementsByTagName()"
+       const ar = []
+       for (const i of document.getElementById('cont').children) {
+         console.log(i.getElementsByClassName('link')[0].innerText)
+         ar.push(i.getElementsByClassName('link')[0].innerText)
+       }
+
+       return ar
+     })
+
+
+     await page.close();
+     return data
+   }
+
+  return  createInstance();
+   
+*/
+  // add tasks to queue
+  var urls = ['j', 'k', 'm']
+  for (let i of urls) {
+    if (i == 'j') {
+      arr.push(createInstance('https://mcubd.netlify.app/'))
+    } else if (i == 'k') {
+      arr.push(createInstance('https://mcubd.netlify.app/marvel/'))
+    } else {
+      arr.push(createInstance('https://mcubd.netlify.app/others'))
+    }
+  }
+  var v = await Promise.all(arr)
+
+
+  return v
+
+
+
 
 
 
@@ -400,81 +400,88 @@ app.post("/", async (req, res) => {
   res.send(await bgfind(req.body));
 });
 
-app.get('/links',async (req,res)=>{
+app.get('/links', async (req, res) => {
   res.send(await bgfind3())
 })
-app.get('/go',async (req,res)=>{
- // request('https://drive.google.com/uc?export=download&id=1PEkrNN4T2ZoqwDrpMU9Oeq8Go8AytqLw', function (error, response, body) {
- // console.log(response.statusCode); 
- // console.log('body:', body);
-   // if( await response.statusCode==200){
-      // res.send(await body)
-   // }i
-    res.sendFile(path.join(__dirname, '/index.js'))
+app.get('/go', async (req, res) => {
+  // request('https://drive.google.com/uc?export=download&id=1PEkrNN4T2ZoqwDrpMU9Oeq8Go8AytqLw', function (error, response, body) {
+  // console.log(response.statusCode); 
+  // console.log('body:', body);
+  // if( await response.statusCode==200){
+  // res.send(await body)
+  // }i
+  res.sendFile(path.join(__dirname, '/index.js'))
 });
-  
 
-app.get('/yt',async (req,res)=>{
+
+app.get('/yt', async (req, res) => {
   await gdrive()
   res.sendFile(path.join(__dirname, '/s.png'))
 })
 
-app.get('/y',async (req,res)=>{
+app.get('/y', async (req, res) => {
 
 
-      chromium.setGraphicsMode = false;
-    const browser = await puppeteer.launch({
+  chromium.setGraphicsMode = false;
+  const browser = await puppeteer.launch({
     args: chromium.args,
     defaultViewport: chromium.defaultViewport,
-    executablePath: await chromium.executablePath("https://github.com/Sparticuz/chromium/releases/download/v110.0.1/chromium-v110.0.1-pack.tar") ,
-     headless: chromium.headless,
-    })
-    
-    const page = await browser.newPage();
-  await page.goto("https://x2mate.com/");
-    await page.waitForTimeout(3000)
-    await page.type('input','url')
-  await page.click('[type==button]')
-  await page.waitForSelector('#btn-action')
-  await page.click('#btn-action')
-  await page.waitForSelector('#asuccess')
-await page.waitForTimeout(3000)
-const data = await page.evaluate(async () => {
-        var divs = document.querySelectorAll('#asuccess')
-        const urls =Array.from(divs).map(v => v.href)
-        return urls
-      })
-  console.log(data)
-
-  
-    const scr = await page.screenshot()  
-
+    executablePath: await chromium.executablePath("https://github.com/Sparticuz/chromium/releases/download/v110.0.1/chromium-v110.0.1-pack.tar"),
+    headless: false
+  })
+ 
+  var ahp="https://www.ssemble.com/en140yn/youtube-downloader/"
+  var ap ="https://save.tube/"
+var aa=req.query.url || "https://youtu.be/QQkmJI63ykI?si=1aOYl9yLULJzSZYh"
+  const page = await browser.newPage();
+  await page.goto(ap )
+  await page.waitForTimeout(3000)
+  await page.screenshot({ path: 'step--11.png' })  
+  await page.type('input[id=video]',aa)
+   await page.click('input[type=submit]')
+   await page.waitForTimeout(6000)
+   //await page.waitForSelector('#buttonDL' , {visible: true})
+   //await page.click('#buttonDL')
+ //  await page.waitForTimeout(3000)
+  //await page.waitForSelector('#cnext')
+ //await page.waitForTimeout(3000)
+ const data = await page.evaluate(async () => {
+   var urlss   = document.querySelectorAll('.downloadBtn')
+   const urls =Array.from(urlss).map(v => v.href)
+   return urls  
+  }) 
+console.log(data)
+ 
+  const scr = await page.screenshot({path :'final.png'})
+ // await page.click('[type=button]')
+  //await page.click('[type=button]')
   await browser.close();
-    res.set('Content-Type', 'image/png');
-    res.send(scr);
-  
+  //res.set('Content-Type', 'image/png');
+  res.send(data );
+  //res.send(data) 
+
   //res.sendFile(path.join(__dirname, '/s.png'))
 })
 
 
 app.get("/uplinks", async (req, res) => {
-  if(JSON.stringify(await bgfind3())[1] == '['){
-  var b= await collec.updateMany({name:'mcubd_links'}, { $set: { links: JSON.stringify( await bgfind3()) ,date:moment().tz('Asia/dhaka').format('h:m a,D/M/YY') } });
-  // console.log(typeof(JSON.stringify( await bgfind3())))
-  res.send(JSON.stringify(b) +' '+  await bgfind3() );
+  if (JSON.stringify(await bgfind3())[1] == '[') {
+    var b = await collec.updateMany({ name: 'mcubd_links' }, { $set: { links: JSON.stringify(await bgfind3()), date: moment().tz('Asia/dhaka').format('h:m a,D/M/YY') } });
+    // console.log(typeof(JSON.stringify( await bgfind3())))
+    res.send(JSON.stringify(b) + ' ' + await bgfind3());
 
-  }else{
+  } else {
     //var b= await collec.updateMany({name:'mcubd_links'}, { $set: { trash: ify(moment().tz('Asia/dhaka').format('h:m a,D/M/YY')+'---' +await bgfind3()) ,date:moment().tz('Asia/dhaka').format('h:m a,D/M/YY') } });
-  res.send('something wrong!!!');
+    res.send('something wrong!!!');
   }
 });
 
 
-app.get('/z',(req,res) => {
- setTimeout(() => {
-   const th = path.join(__dirname, 's.png');
-  res.sendFile(th)
- }, 00);
+app.get('/z', (req, res) => {
+  setTimeout(() => {
+    const th = path.join(__dirname, 's.png');
+    res.send('id: ' + req.query.id);
+  }, 10);
 })
 // console.log(moment().tz('Asia/dhaka').format('h:m a,D/M/YY'))
 
@@ -483,35 +490,35 @@ app.get("/", async (req, res) => {
   res.send('Home gsweat hommop!');
 });
 
-app.get('/fb',async (req,res) => {
-  var g= await fetch(req.query['link'],{
-    method:"GET", 
-    headers:{
+app.get('/fb', async (req, res) => {
+  var g = await fetch(req.query['link'], {
+    method: "GET",
+    headers: {
       // ":authority":"www.facebook.com",
       // ":method":"GET",
       // ":path":"/",
       // ":scheme":"https",
-      "accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-      "accept-encoding":"gzip, deflate, br",
-      "accept-language":"en-GB,en-US;q=0.9,en;q=0.8,bn;q=0.7",
-      "cache-control":"max-age=0",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
-        "sec-ch-ua":'"Not?A_Brand";v="8", "Chromium";v="108", "Google Chrome";v="108"',
-        "sec-ch-ua-platform":"Windows"
+      "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+      "accept-encoding": "gzip, deflate, br",
+      "accept-language": "en-GB,en-US;q=0.9,en;q=0.8,bn;q=0.7",
+      "cache-control": "max-age=0",
+      "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
+      "sec-ch-ua": '"Not?A_Brand";v="8", "Chromium";v="108", "Google Chrome";v="108"',
+      "sec-ch-ua-platform": "Windows"
 
-      }
+    }
   })
 
-console.log()
+  console.log()
 
   res.send(await g.text())
- 
+
 })
 
-async function a(){
-var g= await fetch('https://www.facebook.com/profile.php?id=100007193401802')
+async function a() {
+  var g = await fetch('https://www.facebook.com/profile.php?id=100007193401802')
 
-console.log(await g.text())
+  console.log(await g.text())
 
 }
 
